80e4b074284506964f0f932de3d3712a0a71454d
shagun-code <157958908+shagun-code@users.noreply.github.com>
2026-01-27 20:37:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -1,22 +1,24 @@
+// User function Template for C++
+
 class Solution {
 private:
-    void dfs(int node, vector<int> adjList[], vector<int>& vis) {
+    void dfs(int node, vector<vector<int>> &adjLs, vector<int> &vis) {
         vis[node] = 1;
-        for (auto it : adjList[node]) {
+        for (auto it : adjLs[node]) {
             if (!vis[it]) {
-                dfs(it, adjList, vis);
+                dfs(it, adjLs, vis);
             }
         }
     }
 
 public:
     int numProvinces(vector<vector<int>> adj, int V) {
-        vector<int> adjList[V];
+        vector<vector<int>> adjLs(V);
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
                 if (adj[i][j] == 1 && i != j) {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
                 }
             }
         }
@@ -26,7 +28,7 @@ public:
         for (int i = 0; i < V; i++) {
             if (!vis[i]) {
                 cnt++;
-                dfs(i, adjList, vis);
+                dfs(i, adjLs, vis);
             }
         }
         return cnt;
