dc1d03e9793ae5d48601ea3182db68c352dc409d
shagun-code <157958908+shagun-code@users.noreply.github.com>
2026-02-01 14:39:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -1,36 +1,29 @@
-// User function Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
   public:
     int minimumMultiplications(vector<int>& arr, int start, int end) {
-        if (start == end) return 0; // If start equals end, no multiplications are needed.
-
-        queue<pair<int, int>> q;
-        q.push({start, 0});
+        queue<pair<int,int>> q;
+        q.push({start,0});  // {node, steps}
         
-        int mod = 100000;
-        vector<int> dist(mod, 1e9); 
+        vector<int> dist(100000, 1e9);
         dist[start] = 0;
         
-        while (!q.empty()) {
+        int mod = 100000;
+        
+        while(!q.empty()){
             int node = q.front().first;
             int steps = q.front().second;
             q.pop();
             
-            for (auto it : arr) {
-                int num = (it * node) % mod;
+            for(auto it : arr){
+                int num = (1LL * it * node) % mod;  // use long long to avoid overflow
                 
-                if (steps + 1 < dist[num]) {
+                if(steps + 1 < dist[num]){
                     dist[num] = steps + 1;
-                    if (num == end) return steps + 1; 
                     q.push({num, steps + 1});
                 }
             }
         }
-        return -1; // If no valid path is found, return -1.
+        
+        return (dist[end] == 1e9) ? -1 : dist[end];
     }
 };
-
