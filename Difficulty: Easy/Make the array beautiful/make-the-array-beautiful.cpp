dbaf84387663437e14ef3b5fedbb2b076991a4b2
shagun-code <157958908+shagun-code@users.noreply.github.com>
2026-03-07 11:23:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        stack<int> s;
+        for(int i = 0; i < arr.size(); i++) {
+            if(s.empty()) {
+                s.push(arr[i]);
+            }
+            else if(arr[i] >= 0) {
+                if(s.top() >= 0)
+                    s.push(arr[i]);
+                else
+                    s.pop();
+            }
+            else {
+                if(s.top() < 0)
+                    s.push(arr[i]);
+                else
+                    s.pop();
+            }
+        }
+        
+        vector<int> ans(s.size());
+        int i = s.size() - 1;
+        while(!s.empty()) {
+            ans[i] = s.top();
+            i--;
+            s.pop();
+        }
+        return ans;
+    }
+};
