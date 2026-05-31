4f6d9683d32d8cedb8edec30ec03b1aab1b72b84
shagun-code <157958908+shagun-code@users.noreply.github.com>
2026-05-31 12:01:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countPaths(int V, vector<vector<int>> &edges) {
+        vector<pair<int,int>> adj[V];
+        for (auto it : edges) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(V, 1e9), ways(V, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        int mod = 1e9 + 7;
+
+        while (!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int edW = it.second;
+
+                if (dis + edW < dist[adjNode]) {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dist[adjNode], adjNode});
+                    ways[adjNode] = ways[node];
+                } else if (dis + edW == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[V - 1];
+    }
+};
+
