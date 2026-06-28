aa8b98a39c57d96155abb8be57818022c5e8f46a
shagun-code <157958908+shagun-code@users.noreply.github.com>
2026-06-28 12:24:10 +0530

    Time: 70 ms (54.51%) | Memory: 87.7 MB (58.17%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        int n=nums.size();
+        long long result=0;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]-i;
+        }
+        unordered_map<int,int>mp;
+        mp[nums[0]]=1;
+        for(int j=1;j<n;j++){
+            int countofNums=mp[nums[j]];
+            int totalNumsBeforej=j;
+            int badPairs=totalNumsBeforej-countofNums;
+            result+=badPairs;
+            mp[nums[j]]++;
+        }
+        return result;
+    }
+
+};
